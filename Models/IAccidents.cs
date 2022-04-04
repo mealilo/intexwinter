d4e8bcc4683fc318ee6ea1c4e0ee543c4907b52e
mealilo <90289295+mealilo@users.noreply.github.com>
2022-04-04 11:59:33 -0600

    context and repo

---
+++ Models/IAccidents.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace intex2.Models
+{
+    public interface IAccidents
+    {
+
+        IQueryable<Accident> Accidents { get; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -31,6 +31,8 @@ namespace intex2
             {
                 options.UseMySql(Configuration["ConnectionStrings:AppointmentDBConnection"]);
             });
+
+            services.AddScoped<IAccidents, EFAccidents>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
