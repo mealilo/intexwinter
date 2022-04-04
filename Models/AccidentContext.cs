65b6e32f59460655dbe378df6790e09718d59885
shar4901 <90164014+shar4901@users.noreply.github.com>
2022-04-04 10:32:06 -0600

    connection string, startup connection string, context

---
+++ Models/AccidentContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace intex2.Models
+{
+    public class AccidentContext : DbContext
+    {
+        public AccidentContext()
+        {
+
+        }
+
+        public AccidentContext(DbContextOptions<AccidentContext> options)
+            : base(options)
+        {
+        }
+    }
+}
