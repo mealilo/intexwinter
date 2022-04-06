8b5486bf5ba9a9550263245ba1582b3e645ba99c
skent22 <90283433+skent22@users.noreply.github.com>
2022-04-06 14:58:43 -0600

    Predictor

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -110,9 +110,98 @@ namespace intex2.Controllers
 
         //Onnx
         [HttpGet]
-        public IActionResult Score()
+        public IActionResult Score(int? crashid)
         {
-            return View();
+            if (crashid != null)
+            {
+            Accident accident = repo.Accidents.Single(x => x.CRASH_ID == crashid);
+            CrashSeverityData data = new CrashSeverityData();
+            data.route = Convert.ToSingle(accident.ROUTE);
+            data.milepoint = Convert.ToSingle(accident.ROUTE);
+            data.lat_utm_y = Convert.ToSingle(accident.LAT_UTM_Y);
+            data.long_utm_x = Convert.ToSingle(accident.LONG_UTM_X);
+
+
+                if (accident.PEDESTRIAN_INVOLVED == "TRUE")
+                {
+                    data.pedestrian_involved = 1;
+                }
+                else
+                {
+                    data.pedestrian_involved = 0;
+                }
+
+                if (accident.MOTORCYCLE_INVOLVED == "TRUE")
+                {
+                    data.motorcycle_involved = 1;
+                }
+                else
+                {
+                    data.motorcycle_involved = 0;
+                }
+
+                if (accident.INTERSECTION_RELATED == "TRUE")
+                {
+                    data.intersection_related = 1;
+                }
+                else
+                {
+                    data.intersection_related = 0;
+                }
+
+                if (accident.OVERTURN_ROLLOVER == "TRUE")
+                {
+                    data.overturn_rollover = 1;
+                }
+                else
+                {
+                    data.overturn_rollover = 0;
+                }
+
+                if (accident.OLDER_DRIVER_INVOLVED == "TRUE")
+                {
+                    data.older_driver_involved = 1;
+                }
+                else
+                {
+                    data.older_driver_involved = 0;
+                }
+
+                if (accident.NIGHT_DARK_CONDITION == "TRUE")
+                {
+                    data.night_dark_condition = 1;
+                }
+                else
+                {
+                    data.night_dark_condition = 0;
+                }
+
+                if (accident.DISTRACTED_DRIVING == "TRUE")
+                {
+                    data.distracted_driving = 1;
+                }
+                else
+                {
+                    data.distracted_driving = 0;
+                }
+
+                var result = _session.Run(new List<NamedOnnxValue>
+                {
+                    NamedOnnxValue.CreateFromTensor("float_input", data.AsTensor())
+                });
+                    Tensor<float> score = result.First().AsTensor<float>();
+                    var prediction = new Prediction { PredictedValue = score.First() };
+                    result.Dispose();
+                    ViewBag.Message = "Predicted Score: " + Math.Round(prediction.PredictedValue, 0);
+
+                return View(data);
+            }
+            else
+            {
+                return View();
+            }
+
+
         }
 
         [HttpPost]
