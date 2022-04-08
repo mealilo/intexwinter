ab596571c92fed83fff6a8be02b7b2ab1b889a16
MattCorb <32273733+MattCorb@users.noreply.github.com>
2022-04-07 18:25:59 -0600

    Revert "comment out search"
    This reverts commit 030464da2859ce4dab268afb6892145d5b87d956.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using Microsoft.ML.OnnxRuntime;
 using Microsoft.ML.OnnxRuntime.Tensors;
 using Microsoft.ML.Transforms.Onnx;
@@ -11,7 +12,15 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+//using JqueryDataTables.ServerSide.AspNetCoreWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using static intex2.Models.AuxiliaryModels.DataTableModels;
+using jQueryDatatableServerSideNetCore.Extensions;
+
 
 namespace intex2.Controllers
 {
@@ -36,7 +45,17 @@ namespace intex2.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var allAccident = repo.Accidents.Take(1000);
+            var jsonarray = new List<string>();
+            foreach (Accident a in allAccident)
+            {
+                string json = JsonConvert.SerializeObject(a, Formatting.Indented);
+                jsonarray = jsonarray.Append(json).ToList();
+            }
+
+            //System.IO.File.WriteAllText(@"\Users\intext.json", JsonConvert.SerializeObject(jsonarray));
+            Console.WriteLine(jsonarray);
+            ; return View();
         }
 
 
@@ -72,10 +91,10 @@ namespace intex2.Controllers
 
         //search. This is our backup if we can't get datatables working
 
-        //public IActionResult Search()
-        //{
-        //    return View();
-        //}
+        public IActionResult Search()
+        {
+            return View();
+        }
         //[HttpPost]
         //public IActionResult Search(IFormCollection form)
         //{
@@ -96,7 +115,7 @@ namespace intex2.Controllers
         public IActionResult AdminView()
         {
 
-            List<Accident> AllAccidents = repo.Accidents.Take(100).ToList();
+            List<Accident> AllAccidents = repo.Accidents.OrderByDescending(d => d.CRASH_DATETIME).Take(100).ToList();
             ViewBag.accidents = AllAccidents;
 
             return View();
@@ -107,8 +126,9 @@ namespace intex2.Controllers
         public IActionResult AddEditAccident(int crashid)
         {
 
-            ViewBag.Cities = repo.Accidents.Select(x => x.CITY).Distinct().ToList();
-            ViewBag.Counties = repo.Accidents.Select(x => x.COUNTY_NAME).Distinct().ToList();
+            ViewBag.Cities = repo.Accidents.OrderBy(x => x.CITY).Select(x => x.CITY).Distinct().ToList();
+
+            ViewBag.Counties = repo.Accidents.OrderBy(x => x.COUNTY_NAME).Select(x => x.COUNTY_NAME).Distinct().ToList();
             // if new bowler
             if (crashid == 0)
             {
@@ -122,12 +142,16 @@ namespace intex2.Controllers
             }
 
         }
+
+   
         [Authorize]
         [HttpPost]
         public IActionResult AddEditAccident(Accident accident)
         {
         
-            ViewBag.Cities = repo.Accidents.Select(x => x.CITY).Distinct().ToList();
+            var city = repo.Accidents.OrderBy(x => x.CITY).Select(x => x.CITY).Distinct().ToList();
+
+
             ViewBag.Counties = repo.Accidents.Select(x => x.COUNTY_NAME).Distinct().ToList();
 
             // if we are having issues with models and the db check here cause i removed the model check
@@ -275,6 +299,20 @@ namespace intex2.Controllers
         [HttpPost]
         public IActionResult Survey(CrashSeverityData survey)
         {
+            Random rd = new Random();
+
+            float rand_num = rd.Next(1, 1000000);
+            survey.route = rand_num;
+
+            rand_num = rd.Next(0, 500);
+            survey.milepoint = rand_num;
+
+            rand_num = rd.Next(4000000, 5000000);
+            survey.lat_utm_y = rand_num;
+
+            rand_num = rd.Next(200000, 700000);
+            survey.long_utm_x = rand_num;
+
             var result = _session.Run(new List<NamedOnnxValue>
             {
                 NamedOnnxValue.CreateFromTensor("float_input", survey.AsTensor())
@@ -291,5 +329,87 @@ namespace intex2.Controllers
             return View();
         }
 
+        [HttpGet] public IActionResult testtables()
+        {
+            return View();
+        }
+      [HttpPost]
+        public async Task<ActionResult> AjaxHandler([FromBody] DtParameters dtParameters)
+        {
+           
+            var searchBy = dtParameters.Search?.Value;
+
+            // if we have an empty search then just order the results by Id ascending
+            var orderCriteria = "CRASH_ID";
+            var orderAscendingDirection = true;
+
+            if (dtParameters.Order != null)
+            {
+                // in this example we just default sort on the 1st column
+                orderCriteria = dtParameters.Columns[dtParameters.Order[0].Column].Data;
+                orderAscendingDirection = dtParameters.Order[0].Dir.ToString().ToLower() == "asc";
+            }
+
+            var result = repo.Accidents.AsQueryable();
+
+            if (!string.IsNullOrEmpty(searchBy))
+            {
+                result = result.Where(r => r.CRASH_ID.ToString() != null && r.CRASH_ID.ToString().ToUpper().Contains(searchBy.ToUpper()) ||
+                                           r.CRASH_DATETIME.ToString() != null && r.CRASH_DATETIME.ToString().ToUpper().Contains(searchBy.ToUpper()) ||
+                                           r.CRASH_SEVERITY_ID.ToString() != null && r.CRASH_SEVERITY_ID.ToString().ToUpper().Contains(searchBy.ToUpper()) ||
+                                           r.ROUTE.ToString() != null && r.ROUTE.ToString().ToUpper().Contains(searchBy.ToUpper()) ||
+                                           r.MILEPOINT.ToString() != null && r.MILEPOINT.ToString().ToUpper().Contains(searchBy.ToUpper()) ||
+                                           r.LAT_UTM_Y.ToString() != null && r.LAT_UTM_Y.ToString().ToUpper().Contains(searchBy.ToUpper()) ||
+                                           r.LONG_UTM_X.ToString() != null && r.LONG_UTM_X.ToString().ToUpper().Contains(searchBy.ToUpper()) ||
+                                           r.MAIN_ROAD_NAME != null && r.MAIN_ROAD_NAME.ToUpper().Contains(searchBy.ToUpper()) ||
+                                           r.CITY != null && r.CITY.ToUpper().Contains(searchBy.ToUpper()) ||
+                                           r.COUNTY_NAME != null && r.COUNTY_NAME.ToUpper().Contains(searchBy.ToUpper())
+                                           );
+            }
+
+            result = orderAscendingDirection ? result.OrderByDynamic(orderCriteria, DtOrderDir.Asc) : result.OrderByDynamic(orderCriteria, DtOrderDir.Desc);
+
+            // now just get the count of items (without the skip and take) - eg how many could be returned with filtering
+            var filteredResultsCount = await result.CountAsync();
+            var totalResultsCount = await repo.Accidents.CountAsync();
+
+            return Json(new DtResult<Accident>
+            {
+                Draw = dtParameters.Draw,
+                RecordsTotal = totalResultsCount,
+                RecordsFiltered = filteredResultsCount,
+                Data = await result
+                    .Skip(dtParameters.Start)
+                    .Take(dtParameters.Length)
+                    .ToListAsync()
+            });
+        }
+
+      
+
     }
-}
+    }
+
+
+//CRASH_ID </ td >
+//CRASH_DATETIME </ td >
+//ROUTE </ td >
+//MILEPOINT </ td >
+//LATITUDE </ td >
+//LONGITUDE </ td >
+//MAIN_ROAD_NAME </ td >
+//CITY </ td >
+//COUNTY_NAME </ td >
+//PEDESTRIAN_INVOLVED </ td >
+//WORK_ZONE_RELATED </ td >
+//BICYCLIST_INVOLVED </ td >
+//MOTORCYCLE_INVOLVED </ td >
+//IMPROPER_RESTRAINT </ td >
+//UNRESTRAINED </ td >
+//CRASH_SEVERITY_ID </ td >
+//DUI </ td >
+//INTERSECTION_RELATED </ td >
+//WILD_ANIMAL_RELATED </ td >
+//DOMESTIC_ANIMAL_RELATED </ td >
+//OVERTURN_ROLLOVER </ td >
+//COMMERCIAL_MOTOR_VEH_INVOLVED
